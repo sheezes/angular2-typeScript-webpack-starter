5e3dc06ac2cbe74dd6bd454cda9dcefce48a2fcf
nir kaufman <nirkaufman@gmail.com>
2015-12-16 21:59:29 +0200

    upfrading to angular2 beta

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,23 @@
-import 'zone.js';
+import 'expose?Zone!zone.js';
 import 'reflect-metadata';
-import { Component, bootstrap } from 'angular2/angular2';
+import { bootstrap } from 'angular2/platform/browser';
+import { Component } from 'angular2/core';
 
 @Component({
   selector: 'App',
-  template: '<h1>{{ title }}</h1>'
+  template: `
+    <h1>{{ title }}</h1>
+  `
 })
 
 class App {
 
-  private title:string;
+  public title:string;
 
   constructor() {
-    this.title = "Angular Components"
+    this.title = "Angular2 Application "
   }
+
 }
 
 bootstrap(App);
